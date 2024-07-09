1732c9543caca30ae05768aeeb257a4497ec6295
lixinze777 <xinze002@e.ntu.edu.sg>
2024-07-09 14:27:04 +0800

    remove parallel

--- rag/contriever/answer_questions_with_contriever.py
+++ rag/contriever/answer_questions_with_contriever.py
@@ -1,37 +1,21 @@
-import asyncio
-import aiohttp
 import os
 import json
 import jsonlines
 from tqdm import tqdm
+from openai import OpenAI
 from transformers import AutoTokenizer, AutoModel
 import torch
 import re
 
 # Initialization of the Contriever model
 model_name_contriever = "facebook/contriever"
-
 tokenizer = AutoTokenizer.from_pretrained(model_name_contriever)
 model = AutoModel.from_pretrained(model_name_contriever)
 
+
 api_key = "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"
 model_name = "gpt-4o-2024-05-13"
-
-async def fetch_response(session, api_key, messages):
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "model": model_name,
-        "messages": messages,
-        "temperature": 0
-    }
-    
-    async with session.post(url, headers=headers, json=data) as response:
-        response_data = await response.json()
-        return response_data
+client = OpenAI(api_key = api_key)
 
 def chunk_text(context, chunk_size):
     sentences = re.split(r'(?<=[.!?]) +', context)
@@ -67,7 +51,7 @@ def retrieve_relevant_chunks_contriever(context, question, chunk_size, num_chunk
     return relevant_chunks
 
 
-async def main():
+def main():
     folder_path = '../../datasets/filtered_QA'
     dataset_files = [
         'naturalquestion.jsonl',
@@ -80,45 +64,54 @@ async def main():
         'musique.jsonl',
     ]
 
-    async with aiohttp.ClientSession() as session:
-        for file in dataset_files:
-            print("Start to process: " + file)
-            with jsonlines.open(folder_path + "/" + file) as f:
-                jsonlist = list(f)
-            
-            output_list = []
-            for item in tqdm(jsonlist):
-                questions = item["questions"]
-                context = item["context"]
-                answer = item["answer"]
+    dataset_files = [
+        'sample.jsonl',
+    ]
 
-                predictions = []
-                for question in questions:
-                    # Retrieve relevant chunks for each question using Contriever
-                    chunks = retrieve_relevant_chunks_contriever(context, question, chunk_size=300, num_chunks=5)
-                    combined_chunks = " ".join(chunks)
-                    
-                    q = "From the context: " + combined_chunks + ", answer the question briefly with no explanation. " + question
-                    messages = [{"role": "user", "content": q}]
-                    response_data = await fetch_response(session, api_key, messages)
-                    prediction = response_data['choices'][0]['message']['content']
-                    predictions.append(prediction)
+    for file in dataset_files:
+        print("Start to process: " + file)
+        with jsonlines.open(folder_path + "/" + file) as f:
+            jsonlist = list(f)
+        
+        output_list = []
+        for item in tqdm(jsonlist):
+            questions = item["questions"]
+            context = item["context"]
+            answer = item["answer"]
 
-                output_item = {
-                    "context": context,
-                    "questions": questions,
-                    "num_question": len(questions),
-                    "answer": answer,
-                    "prediction": predictions,
-                    "dataset": file.split(".")[0],
-                }
-                output_list.append(output_item)
+            predictions = []
+            for question in questions:
+                # Retrieve relevant chunks for each question using Contriever
+                chunks = retrieve_relevant_chunks_contriever(context, question, chunk_size=300, num_chunks=5)
+                combined_chunks = " ".join(chunks)
+                
+                q = "From the context: " + combined_chunks + ", answer the question briefly with no explanation. " + question
+                response = client.chat.completions.create(
+                    model=model_name,
+                    temperature = 0,
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": q}
+                    ]
+                )
+                prediction = response.choices[0].message.content
+                predictions.append(prediction)
 
-            output_file = '../../outputs/contriever/gpt-4o/' + file
-            os.makedirs(os.path.dirname(output_file), exist_ok=True)
-            with jsonlines.open(output_file, mode='w') as writer:
-                writer.write_all(output_list)
-            print("Finished processing: " + file)
+            output_item = {
+                "context": context,
+                "questions": questions,
+                "num_question": len(questions),
+                "answer": answer,
+                "prediction": predictions,
+                "dataset": file.split(".")[0],
+            }
+            output_list.append(output_item)
+
+        output_file = '../../outputs/contriever/gpt-4o/' + file
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        with jsonlines.open(output_file, mode='w') as writer:
+            writer.write_all(output_list)
+        print("Finished processing: " + file)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- rag/contriever/answer_questions_with_contriever_mcq.py
+++ rag/contriever/answer_questions_with_contriever_mcq.py
@@ -1,37 +1,21 @@
-import asyncio
-import aiohttp
 import os
 import json
 import jsonlines
 from tqdm import tqdm
+from openai import OpenAI
 from transformers import AutoTokenizer, AutoModel
 import torch
 import re
 
 # Initialization of the Contriever model
 model_name_contriever = "facebook/contriever"
-
 tokenizer = AutoTokenizer.from_pretrained(model_name_contriever)
 model = AutoModel.from_pretrained(model_name_contriever)
 
+
 api_key = "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"
 model_name = "gpt-4o-2024-05-13"
-
-async def fetch_response(session, api_key, messages):
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "model": model_name,
-        "messages": messages,
-        "temperature": 0
-    }
-    
-    async with session.post(url, headers=headers, json=data) as response:
-        response_data = await response.json()
-        return response_data
+client = OpenAI(api_key = api_key)
 
 def chunk_text(context, chunk_size):
     sentences = re.split(r'(?<=[.!?]) +', context)
@@ -55,6 +39,7 @@ def chunk_text(context, chunk_size):
 
     return chunks
 
+
 def retrieve_relevant_chunks_contriever(context, question, chunk_size, num_chunks):
     context_embeddings = [model(**tokenizer(doc, truncation=True, max_length=512, return_tensors='pt'))['last_hidden_state'].mean(dim=1) for doc in chunk_text(context, chunk_size)]
     question_embedding = model(**tokenizer(question, truncation=True, max_length=512, return_tensors='pt'))['last_hidden_state'].mean(dim=1)
@@ -66,7 +51,7 @@ def retrieve_relevant_chunks_contriever(context, question, chunk_size, num_chunk
     return relevant_chunks
 
 
-async def main():
+def main():
     folder_path = '../../datasets/filtered_QA'
     dataset_files = [
         'coursera.jsonl',
@@ -75,46 +60,54 @@ async def main():
         'novelqa.jsonl'
     ]
 
-    async with aiohttp.ClientSession() as session:
-        for file in dataset_files:
-            print("Start to process: " + file)
-            with jsonlines.open(folder_path + "/" + file) as f:
-                jsonlist = list(f)
-            
-            output_list = []
-            for item in tqdm(jsonlist):
-                questions = item["questions"]
-                context = item["context"]
-                answer = item["answer"]
+    dataset_files = [
+        'sample.jsonl',
+    ]
 
-                predictions = []
-                for question in questions:
-                    # Retrieve relevant chunks for each question using Contriever
-                    chunks = retrieve_relevant_chunks_contriever(context, question, chunk_size=300, num_chunks=5)
-                    combined_chunks = " ".join(chunks)
-                    
-                    q = "From the context: " + combined_chunks + ", answer the question with the letters of the correct options (e.g., A, B, C, or D) without including text. " + question
-                    messages = [{"role": "user", "content": q}]
-                    response_data = await fetch_response(session, api_key, messages)
-                    prediction = response_data['choices'][0]['message']['content']
-                    predictions.append(prediction)
+    for file in dataset_files:
+        print("Start to process: " + file)
+        with jsonlines.open(folder_path + "/" + file) as f:
+            jsonlist = list(f)
+        
+        output_list = []
+        for item in tqdm(jsonlist):
+            questions = item["questions"]
+            context = item["context"]
+            answer = item["answer"]
 
-                output_item = {
-                    "context": context,
-                    "questions": questions,
-                    "num_question": len(questions),
-                    "answer": answer,
-                    "prediction": predictions,
-                    "dataset": file.split(".")[0],
-                }
-                output_list.append(output_item)
+            predictions = []
+            for question in questions:
+                # Retrieve relevant chunks for each question using Contriever
+                chunks = retrieve_relevant_chunks_contriever(context, question, chunk_size=300, num_chunks=5)
+                combined_chunks = " ".join(chunks)
+                
+                q = "From the context: " + combined_chunks + ", answer the question with the letters of the correct options (e.g., A, B, C, or D) without including text. " + question
+                response = client.chat.completions.create(
+                    model=model_name,
+                    temperature = 0,
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": q}
+                    ]
+                )
+                prediction = response.choices[0].message.content
+                predictions.append(prediction)
 
-            output_file = '../../outputs/contriever/gpt-4o/' + file
-            os.makedirs(os.path.dirname(output_file), exist_ok=True)
-            with jsonlines.open(output_file, mode='w') as writer:
-                writer.write_all(output_list)
-            print("Finished processing: " + file)
+            output_item = {
+                "context": context,
+                "questions": questions,
+                "num_question": len(questions),
+                "answer": answer,
+                "prediction": predictions,
+                "dataset": file.split(".")[0],
+            }
+            output_list.append(output_item)
 
+        output_file = '../../outputs/contriever/gpt-4o/' + file
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        with jsonlines.open(output_file, mode='w') as writer:
+            writer.write_all(output_list)
+        print("Finished processing: " + file)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
