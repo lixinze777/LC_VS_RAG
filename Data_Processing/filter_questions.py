32a0dd31ec1019e0b78d8a5da2048ea6037338f7
LI XINZE <e0202539@u.nus.edu>
2024-12-26 13:26:25 +0800

    Add files via upload

---
+++ Data_Processing/filter_questions.py
@@ -0,0 +1,71 @@
+import asyncio
+import aiohttp
+import os
+import json
+import jsonlines
+from tqdm import tqdm
+
+api_key = "your key here"
+
+async def fetch_response(session, api_key, messages):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": "gpt-4o-2024-05-13",
+        "messages": messages,
+        "temperature":0
+    }
+    
+    async with session.post(url, headers=headers, json=data) as response:
+        response_data = await response.json()
+        return response_data
+
+async def main():
+
+    folder_path = '../datasets/full_set'
+
+    dataset_files = [
+        '2wikimultihopqa.jsonl',
+        'hotpotqa.jsonl',
+        'multifieldqa.jsonl',
+        'naturalquestion.jsonl',
+        'narrativeqa.jsonl',
+        'multidoc2dial.jsonl',
+        'qasper.jsonl',
+        'musique.jsonl',
+    ]
+
+    for file in dataset_files:
+        print("Start to process: "+file)
+        with jsonlines.open(folder_path+"/"+file) as f:
+            jsonlist = list(f)
+
+        question_list = []
+        for item in jsonlist:
+            for question in item["questions"]:
+                question_list.append(question)
+
+        batch_size = 5
+        for i in tqdm(range(0, len(question_list), batch_size)):
+            batch = question_list[i:i+batch_size]
+            if len(batch) == 0:
+                continue
+            messages_list = [
+                [{"role": "user", "content": "Answer briefly with no explanation, "+qa}] for qa in batch
+            ]
+        
+            async with aiohttp.ClientSession() as session:
+                tasks = [fetch_response(session, api_key, messages) for messages in messages_list]
+                responses = await asyncio.gather(*tasks)
+                
+                for i in range(len(responses)):
+                    question = batch[i]
+                    output = responses[i]['choices'][0]['message']['content']
+                    with open('../outputs/unfiltered_full_set/'+file, 'a') as outfile:
+                        json.dump([question,output], outfile)
+                        outfile.write('\n')
+
+asyncio.run(main())
